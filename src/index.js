38b5dc1e75e25b909f87a325e447c7c01766fac6
Simon Cannon <simoncannon-ceganalytics@outlook.com>
2020-05-24 10:10:11 +0100

    Changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import * as Tone from "tone";
 
+const synth = new Tone.Synth().toMaster();
+
 async function play() {
-    await Tone.start();
-    const synth = new Tone.Synth().toMaster();
     synth.triggerAttackRelease("C4", "2n");
 }
 
+function translateKey(key) {
+    switch (key)
+    {
+        case "a": return "C4";
+        case "s": return "D4";
+        case "d": return "E4";
+        case "f": return "F4";
+        case "g": return "G4";
+        case "h": return "A4";
+        case "j": return "B4";
+        case "k": return "C5";
+        default: return null;
+    }
+}
+
+function startNote(e) {
+    var note = translateKey(e.key);
+    if (note) {
+        synth.triggerAttack(note, Tone.context.currentTime);
+    }
+}
+
+function endNote() {
+    synth.triggerRelease();
+}
+
 document.querySelector("#playButton").addEventListener('click', play);
+
+document.querySelector("body").addEventListener("keydown", startNote);
+document.querySelector("body").addEventListener("keyup", endNote);
